e5c467d1bb4780fd8d227e80bb86336e215daa3d
a2175 <as217512@gmail.com>
2024-12-01 18:15:58 +0900

    init

---
+++ src/main/java/hello/springmsaconfigserver/SpringmsaconfigserverApplication.java
@@ -0,0 +1,13 @@
+package hello.springmsaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsaconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmsaconfigserverApplication.class, args);
+	}
+
+}
