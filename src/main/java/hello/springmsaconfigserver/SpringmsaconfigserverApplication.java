1959c15a463f433d86d7fff71dd2774f7926b329
a2175 <as217512@gmail.com>
2024-12-02 18:35:04 +0900

    스프링 클라우드 MSA 4 : Config 서버 구축
    https://www.youtube.com/watch?v=qeQPBImEGec&list=PLJkjrxxiBSFBPk-6huuqcjiOal1KdU88R&index=4

--- src/main/java/hello/springmsaconfigserver/SpringmsaconfigserverApplication.java
+++ src/main/java/hello/springmsaconfigserver/SpringmsaconfigserverApplication.java
@@ -2,8 +2,10 @@ package hello.springmsaconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringmsaconfigserverApplication {
 
 	public static void main(String[] args) {
